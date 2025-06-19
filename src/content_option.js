2cb7d7e9794dbfa201825bb5e44cdbc32894dc24
Paul Cottrell <pauljamescottrell@gmail.com>
2025-06-19 12:55:35 +0800

    Set up for ghpages

--- src/content_option.js
+++ src/content_option.js
@@ -2,7 +2,7 @@ import * as devData from "./data.dev.js";
 import * as prodData from "./data.prod.js";
 
 //const isDev = process.env.NODE_ENV === "production";
-//const data = isDev ? devData : prodData;
+//const data = isDev ? devData : prodData; FOR LATER WHEN I PUSH THE SITE LIVE
 
 const data = prodData;
 
